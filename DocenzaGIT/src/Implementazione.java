3eac1e1c3c7c3dbbcca1e2cfbb7f306318fa6389
Francesco <Francesco@DESKTOP-H3V1IQ8>
2020-05-05 10:37:15 +0200

    Inserimento nuova stampa

---
+++ DocenzaGIT/src/Implementazione.java
@@ -0,0 +1,12 @@
+
+public class Implementazione {
+
+	public static void main(String[] args) {
+		int i = 27;
+		
+		System.out.println("Il numero in ingresso è: "+ i);
+		System.out.println("OK");
+
+	}
+
+}
